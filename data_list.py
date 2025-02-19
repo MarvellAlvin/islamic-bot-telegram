c34276001b03ab798be78da20324b815d5f4a3eb
Marvell <160994328+MarvellAlvin@users.noreply.github.com>
2025-02-19 23:00:00 +0700

    Create data_list.py

---
+++ data_list.py
@@ -0,0 +1,16 @@
+# data_list.py
+
+list_dzikir = [
+    "Subhanallah, walhamdulillah, wa laa ilaaha illallah, wallahu akbar",
+    "La hawla wa la quwwata illa billah",
+    "Astaghfirullah",
+    "Hasbunallahu wa ni'mal wakeel"
+]
+
+list_renungan = [
+    "Renungkan setiap langkahmu, karena setiap hari adalah kesempatan baru untuk memperbaiki diri.",
+    "Jangan pernah menyerah, karena setiap kegagalan adalah awal dari kesuksesan yang lebih besar.",
+    "Hidup adalah perjalanan, nikmati setiap momen dan teruslah maju.",
+    "Saat menghadapi kesulitan, ingatlah bahwa itu adalah bagian dari proses untuk menjadi lebih kuat.",
+    "Mulailah hari dengan pikiran positif dan hati yang tabah, maka segala rintangan akan terasa lebih ringan."
+]
